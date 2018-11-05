ff8e5b4fb3bcdbbc9ae04edff936078f4e2a505c
Daniela Di Iorio <danielad@gmail.com>
2018-11-05 15:57:06 -0800

    basic json tests passing

--- test/controllers/movies_controller_test.rb
+++ test/controllers/movies_controller_test.rb
@@ -1,19 +1,41 @@
 require "test_helper"
 
 describe MoviesController do
-  it "should get index" do
-    get movies_index_url
-    value(response).must_be :success?
+  let(:movie_one) {movies(:one)}
+
+  describe 'index' do
+
+    it "is a real working route and returns JSON" do
+      get movies_path
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
+    end
+
   end
 
-  it "should get show" do
-    get movies_show_url
-    value(response).must_be :success?
+  describe 'show' do
+
+    it "works with a valid id and returns JSON" do
+      get movie_path(movie_one.id)
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
+    end
+
   end
 
-  it "should get create" do
-    get movies_create_url
-    value(response).must_be :success?
+  describe 'create' do
+
+    it "works with valid data and returns JSON" do
+      post movies_path, params: {
+                                  title: "A movie",
+                                  overview: "a description",
+                                  release_date: Date.today,
+                                  inventory: 5
+                                }
+      expect(response.header['Content-Type']).must_include 'json'
+      must_respond_with :success
+    end
+
   end
 
 end
