b3700eb97d54c1e3151f64dff18fbc2cd6e71229
Daniela Di Iorio <danielad@gmail.com>
2018-11-05 15:18:09 -0800

    moviescontroller methods index, show, and create, tested with postman

--- test/models/rental_test.rb
+++ test/models/rental_test.rb
@@ -1,17 +1,4 @@
 require "test_helper"
-#
-# validates_presence_of :checkout_date
-# validates_presence_of :due_date
-#
-# belongs_to :customer
-# belongs_to :movie
-#
-#
-# def initialize(attributes={})
-#   super
-#   self.active? = true
-# end
-
 
 describe Rental do
   let(:rental_one) {rentals(:one)}
