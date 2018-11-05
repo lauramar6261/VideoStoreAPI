f651722a4a621f09a192c48ddd9145cc29da232f
Daniela Di Iorio <danielad@gmail.com>
2018-11-05 15:38:16 -0800

    wave 2 smoke tests passing

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -18,7 +18,7 @@ class MoviesController < ApplicationController
     movie = Movie.new(movie_params)
 
     if movie.save
-      render json: {ok: true, movie: movie.as_json(only: [:title, :overview, :release_date, :inventory, :available_inventory])}, status: :ok
+      render json: movie.as_json(only: [:id]), status: :ok
     else
       render json: {ok: false, message: movie.errors.messages}, status: :bad_request
     end
