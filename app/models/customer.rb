0cb4e78b9f7c3eb3189837eeafee0b9fefb98bf0
Ada <larmartinez2@gmail.com>
2018-11-05 13:02:42 -0800

    customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  has_many :rentals
+  has_many :movies, through: :rentals
+  validates :name, :registered_at, :address, :city, :state, :postal_code, :phone, presence: true
 end
