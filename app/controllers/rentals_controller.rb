c003a6147a664e7d8d5f25a167dec5f0ce06cca4
Daniela Di Iorio <danielad@gmail.com>
2018-11-06 14:24:04 -0800

    logic in rentalscontroller

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,16 +1,32 @@
 class RentalsController < ApplicationController
   def checkout
     movie = Movie.find_by(id: rental_params[:movie_id])
-
-    customer = Customer.find_by(id: rental_params[:customer_id])
-    checkout_date = Date.today
-    due_date = checkout_date + 7
-    rental = Rental.new(customer: customer, checkout_date: checkout_date, due_date: due_date, movie: movie)
-
-    if rental.save
-      render json: rental.as_json(only: [:id]), status: :ok
+    if !movie
+      render json: {"errors": {"movie": ["Movie not found"]}}, status: :bad_request
     else
-      render json: {ok: false, message: rental.errors.messages}, status: :bad_request
+      customer = Customer.find_by(id: rental_params[:customer_id])
+      if !customer
+        render json: {"errors": {"customer": ["Customer not found"]}}, status: :bad_request
+      else
+        checkout_date = Date.today
+        due_date = checkout_date + 7
+        rental = Rental.new(customer: customer, checkout_date: checkout_date, due_date: due_date, movie: movie)
+
+        if rental.movie.available_inventory > 0
+            if rental.save
+              rental.movie.available_inventory -= 1
+              if !rental.movie.save
+                render json: {"errors": {"movie": rental.movie.errors.messages}}, status: :bad_request
+              else
+                render json: rental.as_json(only: [:id]), status: :ok
+              end
+            else
+              render json: {"errors": {"movie": rental.errors.messages}}, status: :bad_request
+            end
+        else
+          render json: {"errors": {"movie": "#{movie.title} is out of stock"}, status: :bad_request
+        end
+      end
     end
   end
 
