7170d6468ef71921088f46ea11fed1e8ffce853b
Ada <larmartinez2@gmail.com>
2018-11-06 16:17:47 -0800

    pagination completed for customer index action

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -3,8 +3,10 @@ class CustomersController < ApplicationController
     customers = Customer.all
 
     sort_param = params[:sort]
+    page_param = params[:p]
+    num_page_param = params[:n]
+    customers = customers.paginate(:page => page_param, :per_page => num_page_param)
     customers = customers.sort_by{|cust| cust.send(sort_param)} if sort_param && sort_param.in?(%w(name registered_at postal_code))
-
     if customers.nil?
       render json: {"errors": {"customer": ["Customer not found"]}}, status: :not_found
     else
